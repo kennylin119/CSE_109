27f104a8b1bb66a315594ea3057d74e10c9dad32
Kenny Lin <kel422@cupid.cse.lehigh.edu>
2019-10-18 23:27:17 -0400

    finished lab 2

---
+++ lab2/deBits.h
@@ -0,0 +1,8 @@
+#ifndef DEBITS
+#define DEBITS
+
+void print1Byte(uint8_t);    /* accepts a 1 byte value and prints out its bits in memory */
+void print4Bytes(uint32_t);  /* accepts a 4 byte value and prints out its bits in memory */
+void print8Bytes(uint64_t);  /* accepts a 8 byte value and prints out its bits in memory */
+
+#endif
